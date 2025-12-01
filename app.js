6f1ca32cc8e446093149765857e42e0961debc53
ronitclan2019 <53222174+ronitclan2019@users.noreply.github.com>
2025-12-01 15:08:53 +0530

    Refactor app.js by removing logger and routes
    Removed logger middleware and updated root route response. Eliminated '/pizzas' route and adjusted module export.

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
-const express = require('express')
-const app = express()
-const logger = require('morgan')
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(logger('dev'))
-
 app.get('/', (req, res) => {
-    res.status(200)
-    res.send('Working...')
-})
-
-app.get('/pizzas', (req, res) => {
-    res.status(200)
-    res.json({
-        pizzas: [
-            {
-                type: 'pepperoni', 
-                size: 'large'
-            },
-            {
-                type: 'meat lovers',
-                size: 'xlarge'
-            },
-            {
-                type: 'buffalo chicken',
-                size: 'medium'
-            }
-        ]
-    })
-})
+  res.send('Hello from Elastic Beanstalk!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = app
